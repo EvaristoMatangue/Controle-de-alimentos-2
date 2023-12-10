e37e02a2f27ad47d8ba390b36e5c33264aeddadb
EvaristoMatangue <106647194+EvaristoMatangue@users.noreply.github.com>
2023-12-10 08:49:32 +0200

    Update __init__.py
    configurando banco de dados

--- rastreador_de_alimentos/__init__.py
+++ rastreador_de_alimentos/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from .main.routes import main
 from .extensions import db
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
 
     # Configuração do banco de dados
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rastreador_de_alimentos/db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Inicialização das extensões
     db.init_app(app)
-
+    migrate = Migrate(app, db)
+    
 
     # Registro dos blueprints
     app.register_blueprint(main)
